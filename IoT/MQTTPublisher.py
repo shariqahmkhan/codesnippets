e123135c18f07d560ba9bfc4d599379c9cdc22cd
= <=>
2022-05-08 16:00:08 +0200

    remote print statement

--- IoT/MQTTPublisher.py
+++ IoT/MQTTPublisher.py
@@ -3,4 +3,3 @@ c = mqt.Client()
 c.connect("broker.hivemq.com",1883)
 c.publish("joinee", "hi! we are glad to have you on board", retain = True)
 #c.disconnect() this does not let command to be send and before that the program disconnects
-print("done")
