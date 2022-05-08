0aa5d896eb416475834cc6f2510228963155a7d1
shariq <srkstyle29@gmail.com>
2022-05-08 15:41:24 +0200

    added Publisher file

---
+++ IoT/MQTTPublisher.py
@@ -0,0 +1,6 @@
+import paho.mqtt.client as mqt
+c = mqt.Client()
+c.connect("broker.hivemq.com",1883)
+c.publish("joinee", "hi! we are glad to have you on board", retain = True)
+#c.disconnect() this does not let command to be send and before that the program disconnects
+print("done")
