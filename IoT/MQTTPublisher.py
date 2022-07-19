726fff02540f91bc2fa1453cdf8ecb12e729c417
Shariq Ahmed Khan <shariqahmkhan@gmail.com>
2022-07-19 09:31:10 +0200

    Update MQTTPublisher.py

--- IoT/MQTTPublisher.py
+++ IoT/MQTTPublisher.py
@@ -1,5 +1,12 @@
 import paho.mqtt.client as mqt
 c = mqt.Client()
+
+# connect to broker 
+# for remote server: use DNS name
+# for local use local IP address
+
 c.connect("broker.hivemq.com",1883)
+
+
 c.publish("joinee", "hi! we are glad to have you on board", retain = True)
 #c.disconnect() this does not let command to be send and before that the program disconnects
