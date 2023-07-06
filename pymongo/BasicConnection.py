48069b8672e52c76f3e98bbe68a113c9eb20b67f
Shariq Ahmed Khan <shariqahmkhan@gmail.com>
2023-07-07 00:49:22 +0200

    Update BasicConnection.py

--- pymongo/BasicConnection.py
+++ pymongo/BasicConnection.py
@@ -1,3 +1,30 @@
 from pymongo import MongoClient
 
 client = MongoClient("<Connection String>")
+
+# create a DB
+db = client.DBName
+
+# create a collection
+collection = db.Collection
+
+# create a sample data
+item_1 = {
+  "_id" : "U1IT00001",
+  "item_name" : "Blender",
+  "max_discount" : "10%",
+  "batch_number" : "RR450020FRG",
+  "price" : 340,
+  "category" : "kitchen appliance"
+}
+
+item_2 = {
+  "_id" : "U1IT00002",
+  "item_name" : "Egg",
+  "category" : "food",
+  "quantity" : 12,
+  "price" : 36,
+  "item_description" : "brown country eggs"
+}
+
+collection.insert_many([item_1, item_2])
