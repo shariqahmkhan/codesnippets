4505b83c0aa99ae12bdd58ffb915aed98d2c6a5b
Shariq Ahmed Khan <shariqahmkhan@gmail.com>
2024-05-13 09:56:25 +0200

    Update and rename IT/PowerShell.ps1 to IT/PowerShell/GetDotNetFramework.ps1

--- IT/PowerShell/GetDotNetFramework.ps1
+++ IT/PowerShell/GetDotNetFramework.ps1
@@ -1,2 +1,2 @@
-•	To know .Net framework 
+# To know .Net framework 
 Get-ChildItem 'HKLM:\SOFTWARE\conectMicrosoft\NET Framework Setup\NDP' -Recurse | Get-ItemProperty -Name version -EA 0 | Where { $_.PSChildName -Match '^(?!S)\p{L}'} | Select PSChildName, version
